3c440740bf69b983660dfbb4933164f3f865153f
Justin Dreher <dreherjd71@gmail.com>
2024-06-18 11:46:04 -0400

    changed connection file to reflect new DB

--- includes/connection.php
+++ includes/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-$pdo = new PDO('mysql:host=localhost;dbname=cms', 'root', '');
+$pdo = new PDO('mysql:host=localhost;dbname=PhpCms', 'root', '');
 } catch (PDOException $e){
 	exit('Database Error.');
 }
